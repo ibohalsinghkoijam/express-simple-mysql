5a5e51ac5bdc531fd4ceb98a32636a8558c701f0
ibohalsinghkoijam <koijam@olivetech.net>
2019-02-19 16:53:38 +0530

    express without router with mysql

---
+++ main.js
@@ -0,0 +1,104 @@
+// https://www.js-tutorials.com/nodejs-tutorial/node-js-rest-api-add-edit-delete-record-mysql-using-express/
+// function greet(message) {
+// 	return function(name) {
+// 		console.log(message + ' ' + name);
+// 	}
+// }
+
+// greet('Hi')('koijam')
+
+var http = require("http");
+var express = require("express");
+var app = express();
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "olive@123",
+  database: "dummy_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("You are now connected...");
+});
+
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true
+  })
+);
+
+//rest api to get all results
+app.get("/employees", function(req, res) {
+  console.log(req);
+  connection.query("select * from employee", function(error, results, fields) {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+});
+
+app.get("/employees/:id", function(req, res) {
+  connection.query(
+    "select * from employee where id=?",
+    [req.params.id],
+    function(error, results, fields) {
+      if (error) throw error;
+      res.end(JSON.stringify(results));
+    }
+  );
+});
+
+//rest api to create a new record into mysql database
+app.post("/employees", function(req, res) {
+  var postData = req.body;
+  connection.query("INSERT INTO employee SET ?", postData, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+});
+
+//rest api to update record into mysql database
+app.put("/employees", function(req, res) {
+  connection.query(
+    "UPDATE `employee` SET `employee_name`=?,`employee_salary`=?,`employee_age`=? where `id`=?",
+    [
+      req.body.employee_name,
+      req.body.employee_salary,
+      req.body.employee_age,
+      req.body.id
+    ],
+    function(error, results, fields) {
+      if (error) throw error;
+      res.end(JSON.stringify(results));
+    }
+  );
+});
+
+//rest api to delete record from mysql database
+app.delete("/employees", function(req, res) {
+  console.log(req.body);
+  connection.query(
+    "DELETE FROM `employee` WHERE `id`=?",
+    [req.body.id],
+    function(error, results, fields) {
+      if (error) throw error;
+      res.end("Record has been deleted!");
+    }
+  );
+});
+
+var server = app.listen(3000, "127.0.0.1", function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+});
